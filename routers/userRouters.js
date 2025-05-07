89d813dcaefa280701b094a088d8c4e8720c43b9
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-05-07 16:31:12 +0400

    import  express router

---
+++ routers/userRouters.js
@@ -0,0 +1,22 @@
+import express from "express";
+const router = express.Router();
+
+import {
+  getUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserStats,
+} from "../controllers/userController.js";
+
+// იუზერების როუტები
+router.get("/", getUsers); // ყველა იუზერის წამოღება
+router.get("/stats", getUserStats); // იუზერების სტატისტიკა
+router.get("/:id", getOneUser); // კონკრეტული იუზერის წამოღება
+router.post("/", createUser); // იუზერის შექმნა
+router.put("/:id", updateUser); // იუზერის განახლება (სრული)
+router.delete("/:id", deleteUser); // იუზერის წაშლა
+router.patch("/:id", updateUser); // იუზერის განახლება (ნაწილობრივი)
+
+export default router;
