46ba1a5ace54aafeda276bbe4e38e6503e924778
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-05-10 23:29:05 +0400

    CRUD

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,11 +30,13 @@ export const getUsers = async (req, res) => {
 };
 
 export const getOneUser = async (req, res) => {
-  const { id } = req.prisma;
+  const { id } = req.params;
+
   try {
-    const user = await prisma.user.findMany({
+    const user = await prisma.user.findUnique({
       where: { id: Number(id) },
     });
+
     if (!user) {
       return res.status(404).json({
         error: "User not found",
@@ -56,13 +58,22 @@ export const getUserStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-  const { id } = req.prisma;
+  const { id } = req.params;
   const { firstName, lastName, email } = req.body;
   try {
+    const existingUser = await prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!existingUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
     const updatedUser = await prisma.user.update({
       where: { id: Number(id) },
       data: { firstName, lastName, email },
     });
+
     res.json(updatedUser);
   } catch (error) {
     res.status(500).json({ error: "Failed to update user" });
@@ -79,4 +90,23 @@ export const deleteUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Failed to delete user" });
   }
+};
+
+export const searchUsers = (req, res) => {
+  const { search } = req.query;
+
+  const users = [
+    { id: 1, name: "Anka", email: "anka@example.com" },
+    { id: 2, name: "Nika", email: "nika@example.com" },
+  ];
+
+  if (!search) return res.json(users);
+
+  const filtered = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(search.toLowerCase()) ||
+      user.email.toLowerCase().includes(search.toLowerCase())
+  );
+
+  res.json(filtered);
 };
