e939b9bc6821dd4976df4d5cb04150f6ac92e9d6
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-05-12 13:14:11 +0400

    import  bcrypt, jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const prisma = new PrismaClient();
 
 // ყველა ფუნქცია Prisma-ს იყენებს და ექსპორტირდება გარე ფაილებისთვის
