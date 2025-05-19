fe4f3067cf5bea4b72fe572cfad547059a3a164a
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-05-19 16:59:34 +0400

    feat(migration): add roleId column to User table and establish foreign key relationship with roles

---
+++ prisma/migrations/20250519125852_make_relation_between_users_and_roles/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "roleId" INTEGER NOT NULL DEFAULT 2;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
