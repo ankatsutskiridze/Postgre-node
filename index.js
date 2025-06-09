c646002576ad951c8e1ca64849b0850cc9aaf822
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-09 12:42:15 +0400

    feat: remove Swagger middleware and related product documentation

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import productRoutes from "./routers/productRouters.js";
-import swaggerUI from "swagger-ui-express";
-import swaggerSpec from "./middlewares/swagger.js";
 import userRouter from "./routers/userRouters.js";
 
 const app = express();
@@ -11,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 // Routes
 app.get("/", (req, res) => {
   res.json({ message: "მოგესალმებით Express PostgreSQL API-ში" });
