a3fae54e0e823fe258ca3935916fac925ae5b86c
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-11 16:33:59 +0400

    feat(auth): secure product routes with authentication middleware

--- routers/productRouters.js
+++ routers/productRouters.js
@@ -10,15 +10,16 @@ import {
   getCategoryStats,
   buyProduct,
 } from "../controllers/productController.js";
+import { auth } from "../middleware/auth.js";
 
 // მომხმარებლების როუტები
 router.get("/", getProducts);
 router.get("/category-stats", getCategoryStats); // კატეგორიის სტატისტიკა
 router.get("/:id", getOneProduct);
-router.post("/", createProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
-router.patch("/:id", updateProduct);
-router.post("/buyProduct/:id", buyProduct);
+router.post("/", auth, createProduct);
+router.put("/:id", auth, updateProduct);
+router.delete("/:id", auth, deleteProduct);
+router.patch("/:id", auth, updateProduct);
+router.post("/buyProduct/:id", auth, buyProduct);
 
 export default router;
