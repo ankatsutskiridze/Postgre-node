e0416de6f87cdef97db5e6805b40cff6785fd07a
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-13 19:51:09 +0400

    feat(middleware): implement disk storage and file filtering for uploads

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -1,7 +1,32 @@
 import multer from "multer";
 import fs from "fs";
+import path from "path";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const uploadDir = "uploads";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+const filterFile = (req, file, cb) => {
+  const allowedTypes = [".jpg", ".jpeg", ".png"];
+  const extname = path.extname(file.originalname);
+  if (allowedTypes.includes(extname)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: filterFile,
+  limits: { fileSize: 5 * 1024 * 1024 },
+}); // 5MB limit
 
 export default upload;
