ee0e0fd97076d3bd5cbd4c9c7042acf961b4140a
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-14 17:05:31 +0400

    fix(errorhandler): fix super constructor call in AppError class

--- utils/errorhandler.js
+++ utils/errorhandler.js
@@ -1,5 +1,6 @@
 export class AppError extends Error {
   constructor(message, statusCode) {
+    super(message, statusCode); // Call the parent constructor with the error message
     this.message = message;
     this.statusCode = statusCode;
     this.status = statusCode.startsWith("4") || statusCode.startsWith("5");
