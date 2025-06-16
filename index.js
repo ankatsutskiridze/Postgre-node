6147f10b8c002bf0adb659e986441da4ccb05553
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-16 20:29:42 +0400

    refactor(index): add error handling middleware to improve error management

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import productRoutes from "./routers/productRouters.js";
 import userRouter from "./routers/userRouters.js";
+import { handleError } from "./utils/errorhandler.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,7 +14,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRouter);
-
+app.use(handleError);
 // Start server
 app.listen(port, () => {
   console.log(`server is running  ${port} port`);
