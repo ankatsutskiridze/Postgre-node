198668347bc7664feaeb2bb23beba16ca35a898f
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-16 21:03:08 +0400

    refactor(index): import AppError for improved error handling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 import productRoutes from "./routers/productRouters.js";
 import userRouter from "./routers/userRouters.js";
 import { handleError } from "./utils/errorhandler.js";
+import { AppError } from "./utils/errorhandler.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
