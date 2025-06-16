d8ce53e73c0f5cfefe1ac40985c4b80f3eb680aa
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-06-16 19:29:15 +0400

    refactor(productController): remove redundant comment for getProducts function

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 const prisma = new PrismaClient();
 
-// პროდუქტების წამოღება
 async function getProducts(req, res) {
   try {
     const result = await prisma.products.findMany({
